a14c7daf65e7ebaa1e680808c63691d9e81c63cb
btgehr01 <btgehr01@louisville.edu>
2024-07-11 00:08:34 -0400

    [brady] added function

--- api/generic/hello.js
+++ api/generic/hello.js
@@ -2,5 +2,5 @@ const accessToken = process.env.SPOTIFY_ACCESS_TOKEN;
 const playlistId = process.env.SPOTIFY_PLAYLIST_ID;
 
 export function GET(request) {
-  return new Response(`Hello from KY ${accessToken} ${playlistId}`);
+  return new Response(`Hello from KY! ${accessToken} ${playlistId}`);
 }
