5d4637f21bcf9ad6811a010c6d2cf39540d66ff9
btgehr01 <btgehr01@louisville.edu>
2024-07-13 00:15:45 -0400

    [brady] added contact email functionality

--- api/email/email.js
+++ api/email/email.js
@@ -1,3 +1,25 @@
-export function GET(request) {
-  return new Response(`Email Myself with another email account`);
-}
+module.exports = (req, res) => {
+  const { fullName, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: email,
+    to: process.env.MY_EMAIL_USERNAME,
+    subject: `New message from ${fullName}`,
+    text: message,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).send(error.toString());
+    }
+    res.status(200).send("Email sent: " + info.response);
+  });
+};
--- api/generic/hello.js
+++
@@ -1,6 +0,0 @@
-const accessToken = process.env.SPOTIFY_ACCESS_TOKEN;
-const playlistId = process.env.SPOTIFY_PLAYLIST_ID;
-
-export function GET(request) {
-  return new Response(`Hello from KY! ${accessToken} ${playlistId}`);
-}
