6deeb3a8f879af401b103a58667dd6e6cb36e94f
btgehr01 <btgehr01@louisville.edu>
2024-07-15 22:55:17 -0400

    [brady] deleated cors

--- api/email/sendEmail.js
+++ api/email/sendEmail.js
@@ -1,24 +1,6 @@
 import nodemailer from "nodemailer";
 
 export default async (req, res) => {
-  const allowedOrigins = [
-    "https://bradygehrman.vercel.app",
-    "http://localhost:3000",
-  ];
-
-  const origin = req.headers.origin;
-
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
-
   const { fullName, email, message } = req.body;
 
   if (!fullName || !email || !message) {
