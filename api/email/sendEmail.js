4e39264fef192f92bf19611c793e57beb81a0127
btgehr01 <btgehr01@louisville.edu>
2024-07-15 23:42:47 -0400

    [brady] test

--- api/email/sendEmail.js
+++ api/email/sendEmail.js
@@ -21,6 +21,8 @@ export default async (req, res) => {
 
   const { fullName, email, message } = req.body;
 
+  res.status(500).send("Failed to send email. Please try again later.");
+
   if (!fullName || !email || !message) {
     return res.status(400).send("All fields are required.");
   }
