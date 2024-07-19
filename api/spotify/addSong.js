32b1a8ba05611b4d39e5cc0e2f358ebecf82f654
btgehr01 <btgehr01@louisville.edu>
2024-07-19 15:03:40 -0400

    [brady] added unique song check

--- api/spotify/addSong.js
+++ api/spotify/addSong.js
@@ -2,6 +2,23 @@ import axios from "axios";
 
 const playlistId = process.env.SPOTIFY_PLAYLIST_ID;
 
+async function getPlaylistTracks(token) {
+  try {
+    const response = await axios.get(
+      `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return response.data.items.map((item) => item.track.uri);
+  } catch (error) {
+    console.error("Error fetching playlist tracks:", error);
+    throw error;
+  }
+}
+
 export default async function handler(req, res) {
   const { songURI } = req.query;
 
@@ -18,22 +35,31 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: "Failed to get access token." });
   }
 
-  const postData = {
-    uris: [songURI],
-    position: 0,
-  };
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-
   try {
+    const existingTracks = await getPlaylistTracks(token);
+
+    if (existingTracks.includes(songURI)) {
+      return res
+        .status(400)
+        .json({ error: "Song is already in the playlist." });
+    }
+
+    const postData = {
+      uris: [songURI],
+      position: 0,
+    };
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
     await axios.post(
       `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
       postData,
       config
     );
+
     return res.status(200).json({ message: "Song added to playlist." });
   } catch (e) {
     console.error(e);
