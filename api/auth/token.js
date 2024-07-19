d1810e7293b3b855261a9970ec71923d275d0dfb
btgehr01 <btgehr01@louisville.edu>
2024-07-19 14:09:01 -0400

    [brady] added het access token function and edited getSong function

---
+++ api/auth/token.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const clientId = process.env.SPOTIFY_CLIENT_ID;
+const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+
+export default async function getAccessToken() {
+  const tokenUrl = "https://accounts.spotify.com/api/token";
+  const data = new URLSearchParams({ grant_type: "client_credentials" });
+
+  try {
+    const response = await axios.post(tokenUrl, data, {
+      headers: {
+        Authorization: `Basic ${Buffer.from(
+          `${clientId}:${clientSecret}`
+        ).toString("base64")}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+    return response.data.access_token;
+  } catch (error) {
+    console.error("Error getting access token:", error);
+    throw error;
+  }
+}
--- api/spotify/getSongsFromSearch.js
+++ api/spotify/getSongsFromSearch.js
@@ -1,32 +1,36 @@
 import axios from "axios";
-
-const accessToken = process.env.SPOTIFY_ACCESS_TOKEN;
+import getAccessToken from "../auth/token.js";
 
 export default async function handler(req, res) {
   const { searchString } = req.query;
 
-  if (!accessToken) {
-    return res.status(400).json({ error: "Spotify access token is missing." });
+  if (!searchString) {
+    return res.status(400).json({ error: "searchString is required." });
   }
 
-  if (searchString) {
-    try {
-      const response = await axios.get(
-        `https://api.spotify.com/v1/search?q=${encodeURIComponent(
-          searchString
-        )}&type=track&limit=10&include_external=audio`,
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        }
-      );
-      return res.status(200).json(response.data);
-    } catch (e) {
-      console.error(e);
-      return res.status(500).json({ error: "Failed to search for songs." });
-    }
-  } else {
-    return res.status(400).json({ error: "searchString is required." });
+  let token = null;
+
+  try {
+    token = await getAccessToken();
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({ error: "Failed to get access token." });
+  }
+
+  try {
+    const response = await axios.get(
+      `https://api.spotify.com/v1/search?q=${encodeURIComponent(
+        searchString
+      )}&type=track&market=US&limit=20&offset=0`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return res.status(200).json(response.data);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({ error: "Failed to search for songs." });
   }
 }
