e97a19541647a9e3d68231382932cf8d2b80d2a8
btgehr01 <btgehr01@louisville.edu>
2024-07-21 13:40:19 -0400

    [brady] fixed brackets

--- api/auth/callback.js
+++ api/auth/callback.js
@@ -20,57 +20,57 @@ export default async function handler(req, res) {
     console.error("State is null or was altered.");
     const errorMessage = encodeURIComponent(defaultErrorMessage);
     return res.redirect(`${frontend_url}/error?errorMessage=${errorMessage}`);
-  }
-
-  res.setHeader(
-    "Set-Cookie",
-    cookie.serialize("spotify_auth_state", "", {
-      httpOnly: true,
-      secure: true,
-      maxAge: -1,
-      path: "/",
-      domain: ".bradygehrman-api.vercel.app",
-      sameSite: "strict",
-    })
-  );
-
-  try {
-    const params = new URLSearchParams();
-    params.append("code", code);
-    params.append("redirect_uri", redirect_uri);
-    params.append("grant_type", "authorization_code");
-
-    const response = await axios.post(
-      "https://accounts.spotify.com/api/token",
-      params.toString(),
-      {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-          Authorization:
-            "Basic " +
-            Buffer.from(`${clientId}:${clientSecret}`).toString("base64"),
-        },
-      }
-    );
-
-    const { access_token } = response.data;
-
+  } else {
     res.setHeader(
       "Set-Cookie",
-      cookie.serialize("access_token", access_token, {
+      cookie.serialize("spotify_auth_state", "", {
         httpOnly: true,
         secure: true,
-        maxAge: 60 * 60,
+        maxAge: -1,
         path: "/",
         domain: ".bradygehrman-api.vercel.app",
         sameSite: "strict",
       })
     );
 
-    return res.redirect(frontend_url);
-  } catch (error) {
-    console.error("Error exchanging authorization code:", error);
-    const errorMessage = encodeURIComponent(defaultErrorMessage);
-    return res.redirect(`${frontend_url}/error?errorMessage=${errorMessage}`);
+    try {
+      const params = new URLSearchParams();
+      params.append("code", code);
+      params.append("redirect_uri", redirect_uri);
+      params.append("grant_type", "authorization_code");
+
+      const response = await axios.post(
+        "https://accounts.spotify.com/api/token",
+        params.toString(),
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Authorization:
+              "Basic " +
+              Buffer.from(`${clientId}:${clientSecret}`).toString("base64"),
+          },
+        }
+      );
+
+      const { access_token } = response.data;
+
+      res.setHeader(
+        "Set-Cookie",
+        cookie.serialize("access_token", access_token, {
+          httpOnly: true,
+          secure: true,
+          maxAge: 60 * 60,
+          path: "/",
+          domain: ".bradygehrman-api.vercel.app",
+          sameSite: "strict",
+        })
+      );
+
+      return res.redirect(frontend_url);
+    } catch (error) {
+      console.error("Error exchanging authorization code:", error);
+      const errorMessage = encodeURIComponent(defaultErrorMessage);
+      return res.redirect(`${frontend_url}/error?errorMessage=${errorMessage}`);
+    }
   }
 }
