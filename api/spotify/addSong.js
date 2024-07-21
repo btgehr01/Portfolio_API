8f66abb4d5d67e6f679d7d4fbb329c3c529dfeac
btgehr01 <btgehr01@louisville.edu>
2024-07-21 01:37:00 -0400

    [brady] removed redirect from addSong endpoint

--- api/spotify/addSong.js
+++ api/spotify/addSong.js
@@ -37,56 +37,52 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Credentials", "true");
 
   if (req.method === "OPTIONS") {
-    console.log("OPTIONS");
+    console.log("Handling OPTIONS request for CORS");
     return res.status(200).end();
-  } else {
-    const { cookies } = req;
-    const token = cookies["access_token"];
+  }
 
-    if (!token) {
-      console.log("Access token is null");
-      return res.redirect(loginURL);
+  const { cookies } = req;
+  const token = cookies["access_token"];
+
+  if (!token) {
+    console.log("Access token is null");
+    return res.status(401).json({ error: "Unauthorized. Please log in." });
+  }
+
+  const { songURI } = req.body;
+
+  if (!songURI) {
+    return res.status(400).json({ error: "songURI is required." });
+  }
+
+  try {
+    const existingTracks = await getPlaylistTracks(token);
+
+    if (existingTracks.includes(songURI)) {
+      return res
+        .status(400)
+        .json({ error: "Song is already in the playlist." });
     }
 
-    const { songURI } = req.body;
+    const postData = {
+      uris: [songURI],
+      position: 0,
+    };
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
 
-    if (!songURI) {
-      return res.status(400).json({ error: "songURI is required." });
-    }
+    await axios.post(
+      `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
+      postData,
+      config
+    );
 
-    if (!songURI) {
-      return res.status(400).json({ error: "songURI is required." });
-    }
-
-    try {
-      const existingTracks = await getPlaylistTracks(token);
-
-      if (existingTracks.includes(songURI)) {
-        return res
-          .status(400)
-          .json({ error: "Song is already in the playlist." });
-      }
-
-      const postData = {
-        uris: [songURI],
-        position: 0,
-      };
-      const config = {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      };
-
-      await axios.post(
-        `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
-        postData,
-        config
-      );
-
-      return res.status(200).json({ message: "Song added to playlist." });
-    } catch (e) {
-      console.error(e.message);
-      return res.status(500).json({ error: "Failed to add song to playlist." });
-    }
+    return res.status(200).json({ message: "Song added to playlist." });
+  } catch (e) {
+    console.error("Failed to add song to playlist:", e.message);
+    return res.status(500).json({ error: "Failed to add song to playlist." });
   }
 }
